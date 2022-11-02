c2a8518807037cd63f4099cff43c549ee1c5391d
chansei <seinosuke.train@gmail.com>
2022-11-03 04:23:05 +0900

    headlessオプション追加

--- script.py
+++ script.py
@@ -11,6 +11,7 @@ import schedule
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 import api_gmail
@@ -19,12 +20,16 @@ URL_LOGIN = 'https://accounts.google.com/ServiceLogin?hl=ja&passive=true&continu
 URL_PAGE = 'https://docs.google.com/forms/d/e/1FAIpQLScUUrTcQyKcZXwCWS1Cm66MrdIX7j6kZ43_QppFefVqT60-mQ/viewanalytics'
 
 def main():
-    driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
+    # Chromeのウィンドウを表示するなら↓を使う
+    # driver = webdriver.Chrome(executable_path="chromedriver.exe")
     html = get_html(driver, URL_LOGIN)
     if html is not None:
         df = parse_html(html)
     while(True):
-        sleep(10) # 雑なループ
+        sleep(30) # 雑なループ
         html = update_html(driver)
         if html is not None:
             _df = parse_html(html)
@@ -43,12 +48,14 @@ def get_html(driver, url):
     driver.get(url)
     try: 
         # Googleログイン画面
+        print('ログイン画面')
         MAIL = driver.find_element(By.ID,'identifierId')
         MAIL.send_keys(os.environ.get('GMAIL_ADDRESS'))
         btn = driver.find_element(By.ID,'identifierNext')
         btn.click()
         sleep(5)
         # SSOログイン画面
+        print('SSOログイン画面')
         USER = driver.find_element(By.ID,'identifier')
         USER.send_keys(os.environ.get('TUAT_ID'))
         btn = driver.find_element(By.XPATH,'//*[@id="login"]/button')
@@ -59,10 +66,12 @@ def get_html(driver, url):
         btn.click()
         sleep(1)
         # 二段階認証送信
+        print('二段階認証送信')
         btn = driver.find_element(By.XPATH,'//*[@id="send-otp-form"]/button')
         btn.click()
         sleep(5)
         # 二段階認証取得
+        print('二段階認証取得')
         key = api_gmail.get_auth_key()
         print(key)
         object_key = driver.find_element(By.ID,'emailotp')
@@ -71,10 +80,12 @@ def get_html(driver, url):
         btn.click()
         sleep(5)
         # 本人確認
+        print('本人確認')
         btn = driver.find_element(By.XPATH,'//*[@id="view_container"]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button')
         btn.click()
         sleep(1)
         # 投票ページ
+        print('投票ページ')
         driver.get(URL_PAGE)
         sleep(5)
         html = driver.page_source
@@ -85,6 +96,7 @@ def get_html(driver, url):
 
 def update_html(driver):
     try:
+        print('ページ更新')
         driver.refresh()
         sleep(5)
         html = driver.page_source
